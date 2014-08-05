6dab006febe369777154be9e93850b0dab174de2
Wieke Kanters <kantersw@gmail.com>
2014-08-05 14:51:54 +0200

    Added code to the start button in the startbuffer fragment.

--- src/com/dcc/fieldtripbuffer/StartBuffer.java
+++ src/com/dcc/fieldtripbuffer/StartBuffer.java
@@ -7,10 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class StartBuffer extends Fragment {
 	private final Context context;
 
+	/**
+	 * Fragment for starting the BufferService.
+	 *
+	 * @param context
+	 */
 	public StartBuffer(final Context context) {
 		this.context = context;
 	}
@@ -21,14 +27,35 @@ public class StartBuffer extends Fragment {
 		final View rootView = inflater.inflate(R.layout.fragment_startbuffer,
 				container, false);
 		return rootView;
-
 	}
 
+	/**
+	 * Called when you click the Start button.
+	 */
+	@SuppressWarnings("unused")
 	private void startBuffer() {
+		// Create Intent for starting the buffer.
 		final Intent intent = new Intent(context, BufferService.class);
-		intent.putExtra("port", 1972);
-		intent.putExtra("nSamples", 1972);
-		intent.putExtra("nEvents", 1972);
+
+		// Grab port number and add to intent.
+		final EditText editTextPort = (EditText) getView().findViewById(
+				R.id.fragment_startbuffer_port);
+		intent.putExtra("port",
+				Integer.parseInt(editTextPort.getText().toString()));
+
+		// Grab nSamples and add to intent.
+		final EditText editTextnSamples = (EditText) getView().findViewById(
+				R.id.fragment_startbuffer_nSamples);
+		intent.putExtra("nSamples",
+				Integer.parseInt(editTextnSamples.getText().toString()));
+
+		// Grab nEvents and add to intent.
+		final EditText editTextnEvents = (EditText) getView().findViewById(
+				R.id.fragment_startbuffer_nEvents);
+		intent.putExtra("nEvents",
+				Integer.parseInt(editTextnEvents.getText().toString()));
+
+		// Start the buffer.
 		context.startService(intent);
 	}
 }
