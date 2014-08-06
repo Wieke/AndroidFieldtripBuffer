0b1400b9625e6164321a650c1fc990d4cb003603
Wieke Kanters <kantersw@gmail.com>
2014-08-06 13:05:19 +0200

    Created a BufferMonitor thread object and moved some code from BufferService to it.

--- src/com/dcc/fieldtripbuffer/BufferService.java
+++ src/com/dcc/fieldtripbuffer/BufferService.java
@@ -11,14 +11,13 @@ import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import buffer_bci.javaserver.Buffer;
-import buffer_bci.javaserver.FieldtripBufferMonitor;
 
-public class BufferService extends Service implements FieldtripBufferMonitor {
+public class BufferService extends Service {
 
 	private Buffer buffer;
+	private BufferMonitor monitor;
 
 	@Override
 	public IBinder onBind(final Intent intent) {
@@ -32,6 +31,7 @@ public class BufferService extends Service implements FieldtripBufferMonitor {
 	public void onDestroy() {
 		Log.i(C.TAG, "Stopping Buffer Service.");
 		buffer.stopBuffer();
+		monitor.stopMonitoring();
 	}
 
 	@Override
@@ -46,8 +46,10 @@ public class BufferService extends Service implements FieldtripBufferMonitor {
 			// Create a buffer and start it.
 			buffer = new Buffer(port, intent.getIntExtra("nSamples", 100),
 					intent.getIntExtra("nEvents", 100));
-			buffer.addMonitor(this);
+			monitor = new BufferMonitor(this);
+			buffer.addMonitor(monitor);
 			buffer.start();
+			monitor.start();
 			Log.i(C.TAG, "Buffer thread started.");
 
 			// Create Foreground Notification
@@ -90,113 +92,5 @@ public class BufferService extends Service implements FieldtripBufferMonitor {
 			Log.i(C.TAG, "Fieldtrip Buffer Service moved to foreground.");
 		}
 		return START_NOT_STICKY;
-	}
-
-	@Override
-	public void updateClientActivity(final int clientID, final long time) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CLIENT_CLOSED);
-		intent.putExtra(C.DATA_CLIENTID, clientID);
-		intent.putExtra(C.DATA_TIME, time);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Client " + clientID + " active at " + time);
-	}
-
-	@Override
-	public void updateClientError(final int clientID, final int errorType,
-			final long time) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.DATA_CLIENTID, clientID);
-		intent.putExtra(C.DATA_TIME, time);
-
-		if (errorType == FieldtripBufferMonitor.ERROR_PROTOCOL) {
-			intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CLIENT_ERROR_PROTOCOL);
-			Log.i(C.TAG, "Client " + clientID
-					+ " violates network protocol at " + time + " .");
-		} else if (errorType == FieldtripBufferMonitor.ERROR_VERSION) {
-			intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CLIENT_ERROR_VERSION);
-			Log.i(C.TAG, "Client " + clientID + " has wrong version " + time
-					+ " .");
-		} else {
-			intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CLIENT_ERROR_CONNECTION);
-			Log.i(C.TAG, "Client " + clientID
-					+ " disconnected unexpectidly at " + time + " .");
-		}
-
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-	}
-
-	@Override
-	public void updateConnectionClosed(final int clientID) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CLIENT_CLOSED);
-		intent.putExtra(C.DATA_CLIENTID, clientID);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Client " + clientID + " closed connection.");
-	}
-
-	@Override
-	public void updateConnectionOpened(final int clientID, final String adress) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_CONNECTION_COUNT);
-		intent.putExtra(C.DATA_ADRESS, adress);
-		intent.putExtra(C.DATA_CLIENTID, clientID);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Client " + clientID + " opened connection at " + adress);
-	}
-
-	@Override
-	public void updateDataFlushed() {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_DATA_FLUSHED);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Events Flushed");
-	}
-
-	@Override
-	public void updateEventCount(final int count) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_EVENT_COUNT);
-		intent.putExtra(C.DATA_COUNT, count);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Event Count currently " + count);
-	}
-
-	@Override
-	public void updateEventsFlushed() {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_EVENTS_FLUSHED);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Events Flushed");
-	}
-
-	@Override
-	public void updateHeader(final int dataType, final float fSample,
-			final int nChannels) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_HEADER);
-		intent.putExtra(C.DATA_DATATYPE, dataType);
-		intent.putExtra(C.DATA_FSAMPLE, fSample);
-		intent.putExtra(C.DATA_NCHANNELS, nChannels);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "New Header: dataType " + dataType + " fSample " + fSample
-				+ " nCHannels " + nChannels);
-	}
-
-	@Override
-	public void updateHeaderFlushed() {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_HEADER_FLUSHED);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Header Flushed");
-	}
-
-	@Override
-	public void updateSampleCount(final int count) {
-		final Intent intent = new Intent(C.FILTER);
-		intent.putExtra(C.UPDATE_TYPE, C.UPDATE_SAMPLE_COUNT);
-		intent.putExtra(C.DATA_COUNT, count);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		Log.i(C.TAG, "Sample Count currently " + count);
 	}
 }
